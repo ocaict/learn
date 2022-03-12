5211e5d8c7fe507b05408ff54ea9edd7568ceea7
Oluegwu Chigozie <64107819+ocaict@users.noreply.github.com>
2022-03-12 12:33:31 +0100

    Add files via upload

--- js/reset-page.js
+++ js/reset-page.js
@@ -1,4 +1,9 @@
 const resetPasswordForm = document.querySelector(".reset-form")
+const successContainer = document.querySelector(".success")
+let indicator = document.querySelector(".indicator")
+let submitBtnText = document.querySelector(".login-btn")
+let submitBtn = document.querySelector("#submit-btn")
+let loader = document.querySelector(".loader")
 
 resetPasswordForm.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -16,6 +21,10 @@ resetPasswordForm.addEventListener("submit", (e) => {
     let validPassword = passwordRule.test(user.password);
     let validPassword2 = passwordRule.test(user.password2);
     if (user.password === user.password2 && validPassword && validPassword2) {
+        submitBtnText.innerHTML = "Proccesssing..."
+        submitBtn.disabled = true
+        loader.classList.remove("hide")
+
         user.id = id
         fetch("https://ocawebtech.herokuapp.com/reset-password", {
             method: "post",
@@ -28,12 +37,26 @@ resetPasswordForm.addEventListener("submit", (e) => {
         })
             .then(response => response.json())
             .then(result => {
-                console.log(result)
+                if (result.success) {
+                    resetPasswordForm.classList.add("hide")
+                    successContainer.classList.remove("hide")
+                } else {
+                    indicator.innerHTML = result.message
+                    submitBtnText.innerHTML = "Reset Password"
+                    submitBtn.disabled = false
+                    loader.classList.add("hide")
+                }
             })
             .catch(err => {
-                console.log(err)
+                submitBtnText.innerHTML = "Reset Password"
+                submitBtn.disabled = false
+                loader.classList.add("hide")
+
+                indicator.innerHTML = "Unable to Connect with Server"
+                // console.log(err)
             })
     } else {
-        console.log("Password Invalid")
+        indicator.innerHTML = "Password must be at least 6 chareter with Uppercase, special charcter and a number"
+
     }
 })
--- js/reset-password.js
+++ js/reset-password.js
@@ -1,9 +1,21 @@
 const resetForm = document.querySelector(".email-form")
+const successContainer = document.querySelector(".success")
+let indicator = document.querySelector(".indicator")
+let submitBtnText = document.querySelector(".login-btn")
+let submitBtn = document.querySelector("#submit-btn")
+let loader = document.querySelector(".loader")
+
 resetForm.addEventListener("submit", (e) => {
     e.preventDefault()
     const email = resetForm.querySelector("#email").value
+    let emailRule = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+    let validEmail = emailRule.test(email);
 
-    if (email) {
+    if (email && validEmail) {
+        submitBtnText.innerHTML = "Proccesssing..."
+        submitBtn.disabled = true
+        loader.classList.remove("hide")
+
         fetch("https://ocawebtech.herokuapp.com/send-reset-link", {
             method: "post",
             headers: {
@@ -15,11 +27,25 @@ resetForm.addEventListener("submit", (e) => {
         })
             .then(response => response.json())
             .then(result => {
-                console.log(result)
+                if (result.success) {
+                    resetForm.classList.add("hide")
+                    successContainer.classList.remove("hide")
+                    submitBtnText.innerHTML = "Send Me a Link"
+                    submitBtn.disabled = false
+                    loader.classList.add("hide")
+                } else {
+                    indicator.innerHTML = result.message
+                    submitBtnText.innerHTML = "Send Me a Link"
+                    submitBtn.disabled = false
+                    loader.classList.add("hide")
+                }
             })
             .catch(err => {
-                console.log(err)
+                indicator.innerHTML = "Unable to send Email, Check your Network Connection"
             })
+    } else {
+        indicator.innerHTML = "Enter a Valid  Email and Try Again"
+
     }
 })
 
